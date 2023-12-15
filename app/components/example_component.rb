4398289cb5a353159636f508348159b748df7777
Nicholas Martin <nicholas.martin@marketdojo.com>
2023-12-15 15:46:09 +0000

    add files

---
+++ app/components/example_component.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ExampleComponent < ViewComponent::Base
+  erb_template <<-ERB
+    <span title="<%= @title %>"><%= content %></span>
+  ERB
+
+  def initialize(title:)
+    @title = title
+  end
+end
